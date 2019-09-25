cfd5f375600c157dc6e7c12d1abc17b708c40753
Joel Magnusson <joel.magnusso@elev.ga.ntig.se>
2019-09-25 10:01:19 +0200

    Added Login class

--- src/cd.java
+++ src/cd.java
@@ -1,4 +1,39 @@
 
 public class cd {
+	private String Author;
+	private String Voice;
+	private String ID;
+	private String Title;
+	public cd(String Author,String Voice,String ID,String Title) {
+		super();
+		this.Author=Author;
+		this.Voice=Voice;
+		this.ID=ID;
+		this.Title=Title;
+	}
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	public String getVoice() {
+		return Voice;
+	}
+	public void setVoice(String voice) {
+		Voice = voice;
+	}
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
 	
 }
