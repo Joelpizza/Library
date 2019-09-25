fd092e7617c27ae19001dff1cfc5d26a2f54b089
Joel Magnusson <joel.magnusso@elev.ga.ntig.se>
2019-09-25 10:21:16 +0200

    made a window

--- src/Login.java
+++ src/Login.java
@@ -1,9 +1,33 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.UIManager;
 
 public class Login extends JFrame {
-
+	
+	public Login() {
+		super("Login");
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			}
+		catch(Exception missfall) {
+			System.out.print(missfall);
+			}
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setPreferredSize(new Dimension(1200,700));
+		setLayout(new BorderLayout());
+		
+		
+		pack();
+		setVisible(true);
+		setLocationRelativeTo(null);
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		new Login();
 
 	}
 
