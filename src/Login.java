16affb615377ebecee64330e83bdd0eb62cd2995
Joel Magnusson <joel.magnusso@elev.ga.ntig.se>
2019-09-26 08:44:51 +0200

    added cd and book functionalities

--- src/Login.java
+++ src/Login.java
@@ -27,7 +27,6 @@ public class Login extends JFrame implements ActionListener {
 		add(addmedia);
 		pack();
 		setVisible(true);
-		setLocationRelativeTo(null);
 	}
 	
 	
@@ -42,21 +41,41 @@ public class Login extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent arg0) {
 		String command=arg0.getActionCommand();
 		if(command=="add media") {
+			String Author;
+			String ID;
+			String title;
+			String SQL;
+			String SQL2;
 			
 			String MType = JOptionPane.showInputDialog(null,"DVD, CD or Book?");
 			switch(MType){
 			case "DVD":
-				String Director = JOptionPane.showInputDialog(null,"Write directors name");;
-				String ID = JOptionPane.showInputDialog(null,"Write or scan ID");;
-				String title = JOptionPane.showInputDialog(null,"write the title of the DVD");;
-				String SQL = String.format("Insert into dvd(MediaID,Director,Title)"+"VALUES ('%s','%s','%s')",ID,Director,title);
-				String SQL2 = String.format("Insert into media(MediaID,MType)"+"VALUES ('%s','dvd')",ID);
+				String Director = JOptionPane.showInputDialog(null,"Write directors name");
+				ID = JOptionPane.showInputDialog(null,"Write or scan ID");
+				title = JOptionPane.showInputDialog(null,"write the title of the DVD");
+				SQL = String.format("Insert into dvd(MediaID,Director,Title)"+"VALUES ('%s','%s','%s')",ID,Director,title);
+				SQL2 = String.format("Insert into media(MediaID,MType)"+"VALUES ('%s','dvd')",ID);
 				db.execute(SQL);
 				db.execute(SQL2);
 				break;
 			case "CD":
+				Author = JOptionPane.showInputDialog("Write authors name");
+				String Voice = JOptionPane.showInputDialog("Write the name of the voice actor");
+				ID = JOptionPane.showInputDialog("Write or scan ID");
+				title = JOptionPane.showInputDialog("write the title of the CD");;
+				SQL = String.format("Insert into cd(MediaID,Author,Title,Voice)"+"VALUES ('%s','%s','%s','%s')",ID,Author,title,Voice);
+				SQL2 = String.format("Insert into media(MediaID,MType)"+"VALUES ('%s','cd')",ID);
+				db.execute(SQL);
+				db.execute(SQL2);
 				break;
 			case "Book":
+				Author = JOptionPane.showInputDialog(null,"Write authors name");
+				ID = JOptionPane.showInputDialog(null,"Write or scan ID");
+				title = JOptionPane.showInputDialog(null,"write the title of the Book");
+				SQL = String.format("Insert into book(MediaID,Author,Title)"+"VALUES ('%s','%s','%s')",ID,Author,title);
+				SQL2 = String.format("Insert into media(MediaID,MType)"+"VALUES ('%s','book')",ID);
+				db.execute(SQL);
+				db.execute(SQL2);
 				break;
 			default: JOptionPane.showMessageDialog(null, "fel skrivet");	
 			}
